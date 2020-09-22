12f8664ffc080aef1da387a9206f1280ce84e0dc
BuildTools <unconfigured@null.spigotmc.org>
2020-09-22 19:26:21 +0200

    MCreator Merge commit '46f3dba37776238558e3b42a6e54a1237561a324' into 04d8060cc632937d106271cbf2f36811210cc1f3

--- src/main/java/net/mcreator/lotmod/procedures/CosdorzucaniaBulletHitsBlockProcedure.java
+++ src/main/java/net/mcreator/lotmod/procedures/CosdorzucaniaBulletHitsBlockProcedure.java
@@ -1,19 +1,11 @@
 package net.mcreator.lotmod.procedures;
 
-import net.minecraft.world.IWorld;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.state.IProperty;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.BlockState;
-
-import net.mcreator.lotmod.LotmodModElements;
-
-import java.util.Map;
-
 @LotmodModElements.ModElement.Tag
 public class CosdorzucaniaBulletHitsBlockProcedure extends LotmodModElements.ModElement {
+
 	public CosdorzucaniaBulletHitsBlockProcedure(LotmodModElements instance) {
 		super(instance, 56);
+
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
@@ -37,20 +29,27 @@ public class CosdorzucaniaBulletHitsBlockProcedure extends LotmodModElements.Mod
 				System.err.println("Failed to load dependency world for procedure CosdorzucaniaBulletHitsBlock!");
 			return;
 		}
+
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
+
 		{
 			BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
 			BlockState _bs = Blocks.WITHER_ROSE.getDefaultState();
+
 			BlockState _bso = world.getBlockState(_bp);
 			for (Map.Entry<IProperty<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
 				IProperty _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
 				if (_bs.has(_property))
 					_bs = _bs.with(_property, (Comparable) entry.getValue());
 			}
+
 			world.setBlockState(_bp, _bs, 3);
+
 		}
+
 	}
+
 }
--- src/main/java/net/mcreator/lotmod/procedures/DiamondlogBlockDestroyedByExplosionProcedure.java
+++ src/main/java/net/mcreator/lotmod/procedures/DiamondlogBlockDestroyedByExplosionProcedure.java
@@ -1,18 +1,11 @@
 package net.mcreator.lotmod.procedures;
 
-import net.minecraft.world.IWorld;
-import net.minecraft.item.ItemStack;
-import net.minecraft.entity.item.ItemEntity;
-import net.minecraft.block.Blocks;
-
-import net.mcreator.lotmod.LotmodModElements;
-
-import java.util.Map;
-
 @LotmodModElements.ModElement.Tag
 public class DiamondlogBlockDestroyedByExplosionProcedure extends LotmodModElements.ModElement {
+
 	public DiamondlogBlockDestroyedByExplosionProcedure(LotmodModElements instance) {
 		super(instance, 57);
+
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
@@ -36,14 +29,18 @@ public class DiamondlogBlockDestroyedByExplosionProcedure extends LotmodModEleme
 				System.err.println("Failed to load dependency world for procedure DiamondlogBlockDestroyedByExplosion!");
 			return;
 		}
+
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
+
 		if (!world.getWorld().isRemote) {
 			ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(Blocks.DIAMOND_BLOCK, (int) (1)));
 			entityToSpawn.setPickupDelay(10);
 			world.addEntity(entityToSpawn);
 		}
+
 	}
+
 }
