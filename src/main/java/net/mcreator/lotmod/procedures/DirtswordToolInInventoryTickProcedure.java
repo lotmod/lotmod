72ef3d072949aef68edacc86946944e2755e07cb
BuildTools <unconfigured@null.spigotmc.org>
2020-09-22 18:44:45 +0200

    MCreator Merge commit '46f3dba37776238558e3b42a6e54a1237561a324' into eed6d8dc2b75d2cebeafb6fb41b27bdfb5a80e34

--- src/main/java/net/mcreator/lotmod/procedures/DirtswordToolInInventoryTickProcedure.java
+++ src/main/java/net/mcreator/lotmod/procedures/DirtswordToolInInventoryTickProcedure.java
@@ -1,16 +1,11 @@
 package net.mcreator.lotmod.procedures;
 
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.Entity;
-
-import net.mcreator.lotmod.LotmodModElements;
-
-import java.util.Map;
-
 @LotmodModElements.ModElement.Tag
 public class DirtswordToolInInventoryTickProcedure extends LotmodModElements.ModElement {
+
 	public DirtswordToolInInventoryTickProcedure(LotmodModElements instance) {
 		super(instance, 54);
+
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
@@ -19,8 +14,12 @@ public class DirtswordToolInInventoryTickProcedure extends LotmodModElements.Mod
 				System.err.println("Failed to load dependency entity for procedure DirtswordToolInInventoryTick!");
 			return;
 		}
+
 		Entity entity = (Entity) dependencies.get("entity");
+
 		if (entity instanceof PlayerEntity)
 			((PlayerEntity) entity).getFoodStats().setFoodLevel((int) 100);
+
 	}
+
 }
