abb6faa1e648d1c9f35010a4782ca5861cf4b4bd
BuildTools <unconfigured@null.spigotmc.org>
2020-09-22 20:49:01 +0200

    MCreator Merge commit '46f3dba37776238558e3b42a6e54a1237561a324' into f121c5237af6d4ab49008305f0593baa630b0814

--- src/main/java/net/mcreator/lotmod/block/DiamondlogBlock.java
+++ src/main/java/net/mcreator/lotmod/block/DiamondlogBlock.java
@@ -1,46 +1,17 @@
 
 package net.mcreator.lotmod.block;
 
-import net.minecraftforge.registries.ObjectHolder;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.common.IPlantable;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.api.distmarker.Dist;
-
-import net.minecraft.world.storage.loot.LootContext;
-import net.minecraft.world.IWorldReader;
-import net.minecraft.world.IBlockReader;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.Rotation;
-import net.minecraft.util.Direction;
-import net.minecraft.state.StateContainer;
-import net.minecraft.state.DirectionProperty;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Item;
-import net.minecraft.item.BlockItemUseContext;
-import net.minecraft.item.BlockItem;
-import net.minecraft.client.renderer.RenderTypeLookup;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.block.material.MaterialColor;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.FallingBlock;
-import net.minecraft.block.DirectionalBlock;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Block;
-
-import net.mcreator.lotmod.itemgroup.LotmodcrItemGroup;
-import net.mcreator.lotmod.LotmodModElements;
-
-import java.util.List;
-import java.util.Collections;
 
 @LotmodModElements.ModElement.Tag
 public class DiamondlogBlock extends LotmodModElements.ModElement {
+
 	@ObjectHolder("lotmod:diamondlog")
 	public static final Block block = null;
+
 	public DiamondlogBlock(LotmodModElements instance) {
 		super(instance, 57);
+
 	}
 
 	@Override
@@ -54,12 +25,19 @@ public class DiamondlogBlock extends LotmodModElements.ModElement {
 	public void clientLoad(FMLClientSetupEvent event) {
 		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
 	}
+
 	public static class CustomBlock extends FallingBlock {
+
 		public static final DirectionProperty FACING = DirectionalBlock.FACING;
+
 		public CustomBlock() {
-			super(Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(1f, 10f).lightValue(0).doesNotBlockMovement()
-					.notSolid());
+			super(
+
+					Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(1f, 10f).lightValue(0).doesNotBlockMovement()
+							.notSolid());
+
 			this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.SOUTH));
+
 			setRegistryName("diamondlog");
 		}
 
@@ -129,10 +107,13 @@ public class DiamondlogBlock extends LotmodModElements.ModElement {
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
+
 	}
+
 }
